28615f4395bb1383d56fca0efab94a9833a6fd8f
Nadiya <nadiya.abdi@zoopla.co.uk>
2020-07-08 12:20:22 +0100

    first step

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+namespace Email_Extraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var content = File.ReadAllText ("sample.txt");
+            Console.WriteLine(content);
+        }
+    }
+}
